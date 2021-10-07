40cd9eaa5ec44d02eceb843bd5678485479f6060
heuirr22 <spris416@gmail.com>
2021-10-08 03:37:26 +0900

    2.3

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
 import React from 'react';
 
-//props를 name, picture를 만들고 활용하는 방법 적음
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 //오브젝트 나열
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+//props를 name, picture를 만들고 활용하는 방법 적음
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 // 오브젝트의 map사용하여 dish에 담고, dish에 각 props를 붙여 Food 컴포넌트에 대입
 function App() {
   return (
     <div>
     {foodILike.map(dish => (
-      <Food name={dish.name} picture={dish.image} />
+      <Food key={dish.id} name={dish.name} picture={dish.image} />
     ))}
   </div>
   );
