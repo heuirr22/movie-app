2a6c57f18214bdccf31ebb6cf8e552fc0e293ecd
heuirr22 <spris416@gmail.com>
2021-10-08 14:05:24 +0900

    3.0

--- src/App.js
+++ src/App.js
@@ -1,77 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//오브젝트 나열
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4.8
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.2
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 2.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 3.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.3
-  }
-];
-
-//props를 name, picture, rating를 만들고 활용하는 방법 적음
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-//propTypes로 속성검사
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-// 오브젝트의 map사용하여 dish에 담고, dish에 각 props를 붙여 Food 컴포넌트에 대입
-function App() {
-  return (
-    <div>
-    {foodILike.map(dish => (
-      <Food
-        key={dish.id}
-        name={dish.name}
-        picture={dish.image}
-        rating={dish.rating}
-      />
-    ))}
-  </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  Add = () => {
+    console.log("Add");
+  };
+  Minus = () => {
+    console.log("Minus");
+  };
+  render() {
+    return(
+      <div>
+        <h1>The number is: {this.state.count} </h1>
+        <button onClick = {this.Add} >Add</button>
+        <button onClick = {this.Minus}>Minus</button>
+      </div>
+    )
+  };
 }
 
 export default App;
