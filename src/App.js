35afdee87c159a70ae24a0bbb4712d9e2b5cb1c1
heuirr22 <spris416@gmail.com>
2021-10-08 20:35:17 +0900

    4.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
   state = {
@@ -6,18 +8,44 @@ class App extends React.Component{
     movies: []
   };
 
+  // 아래처럼 하면 예쁘지 않으므로, ES6 사용 권장
+  // getMovies = async () => {
+  //  const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  //  console.log(movies.data.data.movies);
+  //};
+
+  getMovies = async () => {
+    const {
+      data : {
+        data : { movies }
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"); //rating 순으로 정렬
+    this.setState({movies, isLoading: false});
+  };
+  
+  // 컴포넌트가 마운트 되면 아래 함수 
   componentDidMount() {
-    // 5초 기다렸다가 isLoading을 false로 변경
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 5000);
+    this.getMovies();
   }
 
   render() {
-    // const로 this.state 빼기
-    const {isLoading} = this.state;
-    // js 삼항연산자
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+              ))}
+              </div>
+            );
   }
 }
 
